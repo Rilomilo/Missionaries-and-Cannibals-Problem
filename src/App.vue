d08bbb72cbc10a9ebe94a73027c8c24072d0ab29
Rilomilo <269797689@qq.com>
2020-08-01 11:06:06 +0800

    webpack初始环境

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        自己搭建的webpack环境
+    </div>
+</template>
+
+<script>
+import Vue from "vue"
+
+export default {
+    data() {
+        return {}
+    },
+    methods: {}
+}
+</script>
+
+<style lang="less">
+    #app{
+        background-color: aqua;
+    }
+</style>
---
+++ webpack.dev.js
@@ -0,0 +1,115 @@
+const { resolve } = require("path")
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
+const VueLoaderPlugin = require("vue-loader/lib/plugin")
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        // filename: 'js/[name]-[contenthash:10].js',
+        filename: 'js/[name]-[hash].js',
+        path: resolve(__dirname, 'dist'),
+        // chunkFilename:'js/[name]-chunk-[hash].js'
+    },
+    mode: 'development',
+    devtool: 'cheap-module-eval-source-map',
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'),
+            vue$: 'vue/dist/vue.runtime.esm.js'
+        },
+        extensions:['.js','.vue'],
+        modules:[
+            resolve(__dirname,'node_modules')
+        ]
+    },
+    optimization:{
+        splitChunks:{
+            chunks:'all',
+            minSize: 30 * 1024, //30KB
+            maxSize:1024*1024 //1MB
+        }
+    },
+    module: {
+        rules: [{
+            test:/\.html$/,
+            loader:'html-loader'
+        },
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader', 'postcss-loader']
+        },
+        {
+            test: /\.less$/,
+            use: ['style-loader', 'css-loader', 'postcss-loader', 'less-loader']
+        },
+        {
+            test: /\.vue$/,
+            loader: 'vue-loader'
+        },
+        {
+            test: /\.(jpg|jpeg|svg|png|gif)$/,
+            loader: 'url-loader',
+            options: {
+                limit: 8 * 1024, // 小于8kB的base64编码处理
+                name: 'image/[name]-[hash:10].[ext]'
+            }
+        },
+        {
+            test: /\.(woff|eot|ttf|otf)$/,
+            loader:'url-loader',
+            options:{
+                limit: 16*1024,
+                name:'font/[name]-[hash:10].[ext]'
+            }
+        }]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+            inject: true
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode:'static',
+            analyzerHost:'127.0.0.1',
+            analyzerPort:8888,
+            openAnalyzer:false
+        }),
+        new webpack.DllReferencePlugin({
+            manifest:resolve(__dirname,'dll/manifest.json')
+        }),
+        new AddAssetHtmlPlugin([
+            {
+                filepath: resolve('./dll/*.js'),
+                outputPath:'js/',
+                publicPath:'js/'
+            },
+            {
+                typeOfAsset:'css',
+                filepath: resolve('./dll/*.css'),
+                outputPath:'css/',
+                publicPath:'css/',
+                hash:true
+            }
+        ]),
+        new webpack.NamedModulesPlugin(),
+        new FriendlyErrorsPlugin()
+    ],
+    devServer:{
+        contentBase:resolve(__dirname,'dist'),
+        watchContentBase:true,
+        hot:true,
+        proxy:{
+            '/gateway':{
+                target:'https://m.maizuo.com',
+                changeOrigin:true
+            }
+        }
+    }
+}
