3e5ebf46c27d9eb28e7315016ac26fb302b6b8e6
Rilomilo <269797689@qq.com>
2021-06-04 09:41:38 +0800

    增加注释，实验报告版本

--- src/SearchController.ts
+++ src/SearchController.ts
@@ -3,8 +3,8 @@ import Status from "./Status"
 export default class SearchController{
     public n:number; 
     public k:number; // 每次最多移动
-    public visited_status_ls:Status[]=[];
-    private _result: Status[][] =[];
+    public visited_status_ls:Status[]=[]; // 存放当前方案中已经访问过的状态
+    private _result: Status[][] =[]; // 存放所有可行的方案，每种方案由若干个状态组成
 
     constructor(n: number, k: number) {
         this.n = n;
@@ -14,6 +14,10 @@ export default class SearchController{
         this.dfs(s);
     }
 
+    /**
+     * 判断当前状态是否已经访问过
+     * @param s 状态
+     */
     public isVisited(s:Status):boolean{
         for(let i=0;i<this.visited_status_ls.length;i++){
             if(s.equals(this.visited_status_ls[i])) return true;
@@ -21,6 +25,10 @@ export default class SearchController{
         return false;
     }
 
+    /**
+     * 从当前状态开始搜索所有合法的走法
+     * @param s 当前状态
+     */
     public dfs(s:Status){
         if(s.cr==this.n && s.mr==this.n){
             let t=[];
@@ -45,6 +53,7 @@ export default class SearchController{
                     let s1:Status=s.move(m,c);
 
                     if(s1.isLegal() && !this.isVisited(s1)){
+                        // 如果生成的新状态是一个合法的状态，并且没有访问过，则可以继续新状态继续搜索
                         this.visited_status_ls.push(s1);
                         this.dfs(s1);
                         this.visited_status_ls.pop();
